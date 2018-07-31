9f7a4b600e8a78d8f823c6c01c54402daf36a999
laura <laura.canelada.velasco@alumnos.upm.es>
2018-07-31 20:46:46 +0200

    añadidos comentarios

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.utils import timezone
+
+#clase post
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
@@ -15,3 +17,20 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+#clase comentarios
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', related_name='comments')
+    author = models.CharField(max_length=100)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
+def approved_comments(self):
+    return self.comments.filter(approved_comment=True)
